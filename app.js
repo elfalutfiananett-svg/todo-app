1f766ada302ea8c6475f86dda6d9fdbb8e3ee900
elfalutfiananett-svg <elfalutfiana.nett@gmail.com>
2026-05-11 13:21:27 +0700

    deploy railway

--- app.js
+++ app.js
@@ -18,11 +18,44 @@ const bucket = 'todoapp';
 
 let tasks = [];
 
+async function loadTasksFromMinio() {
+
+    try {
+
+        const dataStream = await minioClient.getObject(
+            bucket,
+            'tasks.json'
+        );
+
+        let data = '';
+
+        dataStream.on('data', chunk => {
+            data += chunk;
+        });
+
+        dataStream.on('end', () => {
+
+            if (data) {
+                tasks = JSON.parse(data);
+            }
+
+        });
+
+    } catch (err) {
+
+        console.log('Belum ada tasks.json');
+
+    }
+}
+
+loadTasksFromMinio();
+
 app.get('/tasks', (req, res) => {
     res.json(tasks);
 });
 
 app.post('/tasks', async (req, res) => {
+
     const task = {
         id: Date.now(),
         text: req.body.text,
@@ -42,6 +75,56 @@ app.post('/tasks', async (req, res) => {
     res.json(task);
 });
 
-app.listen(3000, () => {
-    console.log('Server berjalan di port 3000');
+app.put('/tasks/:id', async (req, res) => {
+
+    const id = Number(req.params.id);
+
+    tasks = tasks.map(task => {
+
+        if (task.id === id) {
+            task.done = true;
+        }
+
+        return task;
+    });
+
+    const data = JSON.stringify(tasks);
+
+    await minioClient.putObject(
+        bucket,
+        'tasks.json',
+        data
+    );
+
+    res.json({
+        message: 'Task selesai'
+    });
+
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+
+    const id = Number(req.params.id);
+
+    tasks = tasks.filter(task => task.id !== id);
+
+    const data = JSON.stringify(tasks);
+
+    await minioClient.putObject(
+        bucket,
+        'tasks.json',
+        data
+    );
+
+    res.json({
+        message: 'Task dihapus'
+    });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di port ${PORT}`);
+
 });
